207babd866ce4deecb9a46647511fa767de1c01a
Scott Shimp <scott.shimp@gmail.com>
2016-01-24 21:30:36 -0500

    Updated files

---
+++ _config.py
@@ -0,0 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True 
+SECRET_KEY = '\x002\x05F\x0c\xd3\xf8Z\x17\x8aXZ\xdc,6\xb3\x16\xebV\xb2\xfa\xbe\xa1\xe1'
+
+DATABASE_PATH = os.path.join(basedir, DATABASE)
