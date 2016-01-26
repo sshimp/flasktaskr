c7a48fb9ba3b0e6462c0fb1555f77be476bb449d
Scott Shimp <scott.shimp@gmail.com>
2016-01-25 21:10:43 -0500

    Added SQLAlchemy

--- _config.py
+++ _config.py
@@ -9,3 +9,5 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = '\x002\x05F\x0c\xd3\xf8Z\x17\x8aXZ\xdc,6\xb3\x16\xebV\xb2\xfa\xbe\xa1\xe1'
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
--- db_create.py
+++ db_create.py
@@ -1,25 +1,34 @@
 # project/db_create.py
 
-import sqlite3
-from _config import DATABASE_PATH
+# import sqlite3
+# from _config import DATABASE_PATH
 
-with sqlite3.connect(DATABASE_PATH) as connection:
+from views import db
+from models import Task
+from datetime import date
 
-	c = connection.cursor()
+# with sqlite3.connect(DATABASE_PATH) as connection:
 
-	c.execute("""
-		create table tasks(
-			task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-			name TEXT NOT NULL,
-			due_date TEXT NOT NULL,
-			priority INTEGER NOT NULL,
-			status INTEGER NOT NULL
-			)""")
+# 	c = connection.cursor()
 
-	c.execute("""insert into tasks (name, due_date, priority, status)
-				values ('Finish this tutorial!','03/25/2015',10,1)""")
+# 	c.execute("""
+# 		create table tasks(
+# 			task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+# 			name TEXT NOT NULL,
+# 			due_date TEXT NOT NULL,
+# 			priority INTEGER NOT NULL,
+# 			status INTEGER NOT NULL
+# 			)""")
 
-	c.execute("""insert into tasks (name, due_date, priority, status)
-				values ('Finish Real Python Course Part II','03/25/2015',10,1)""")
+# 	c.execute("""insert into tasks (name, due_date, priority, status)
+# 				values ('Finish this tutorial!','03/25/2015',10,1)""")
 
-	
+# 	c.execute("""insert into tasks (name, due_date, priority, status)
+# 				values ('Finish Real Python Course Part II','03/25/2015',10,1)""")
+
+db.create_all()
+
+db.session.add(Task("Finish this tutorial",date(2015,3,13),10,1))
+db.session.add(Task("Finish Real Python",date(2015,3,13),10,1))
+
+db.session.commit()
